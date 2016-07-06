7c481d56b405eb0a161be75d9a0c97fb150b8148
fizLBQ <1848406889@qq.com>
2016-07-06 22:29:24 +0800

    demo

--- BitucketSpider.py
+++ BitucketSpider.py
@@ -1,36 +1,37 @@
-# coding:utf-8
+#coding:utf-8
 
 
 
 import requests
 from bs4 import BeautifulSoup
 
-USERNAME = 'FizLBQ'
+USERNAME = 'fizlbq@gmail.com'
 PWD = 'Ll139196'
 
-LoginUrl_GET = 'https://github.com/login'
-LoginUrl = 'https://github.com/session'
+# LoginUrl = 'https://bitbucket.org/account/signin/'
+LoginUrl = 'https://bitbucket.org/account/signin/'
 
 headers = {
-    'Host': 'github.com',
+    'Host': 'bitbucket.org',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://github.com',
+    'Referer': 'https://bitbucket.org/account/signin/?next=/',
     'Connection': 'keep-alive',
 }
 
 formData = {
-    'commit': 'Sign+in',
-    'utf8': "✓",
-    "login": USERNAME,
+    'next': '/',
+    # 'csrfmiddlewaretoken': "csrfmiddlewaretoken",
+    "username": USERNAME,
     "password": PWD,
 
+
 }
-s = requests.Session()
+s=requests.Session()
 # RESULT = s.get(LoginUrl, headers=headers,data=formData)
-RESULT = s.get(LoginUrl_GET, headers=headers)
+RESULT = s.get(LoginUrl, headers=headers)
 print
 
 content = RESULT.content
@@ -39,19 +40,20 @@ with open('login.html', 'wb') as fp:
     fp.write(content)
 html = open('login.html', 'r')
 soup = BeautifulSoup(html, "html.parser")
-token = soup.find('input', {'name': 'authenticity_token'})['value']
+token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
 
 print token
-formData['authenticity_token'] = token
+formData['csrfmiddlewaretoken']=token
+
+
+
 
 ''' cooke'''
-import pickle
-
+import  pickle
 
 def save_cookies(requests_cookiejar, filename):
     with open('cookies.txt', 'wb') as f:
         pickle.dump(requests_cookiejar, f)
-
 
 def load_cookies(filename):
     with open(filename, 'rb') as f:
@@ -60,9 +62,15 @@ def load_cookies(filename):
 
 save_cookies(s.cookies, 'cookies.txt')
 
-RESULT = s.post(LoginUrl, headers=headers, data=formData,cookies=load_cookies('cookies.txt') )
-content=RESULT.content
+
+
+
+
+
+
+RESULT=s.post(LoginUrl, headers=headers,data=formData,cookies=load_cookies('cookies.txt'))
 print RESULT.url
+
 
 print RESULT.status_code
 print RESULT.cookies
@@ -70,12 +78,11 @@ print RESULT.cookies
 with open('test.html', 'w') as fp:
     fp.write(content)
 
+
 html = open('test.html', 'r')
 soup = BeautifulSoup(html, "html.parser")
-token = soup.find('input', {'name': 'authenticity_token'})['value']
-RESULT = s.post(LoginUrl, headers=headers, data=formData,cookies=RESULT.cookies )
-print RESULT.url
-
+token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+RESULT=s.post(LoginUrl, headers=headers,data=formData,)
 print RESULT.status_code
 print RESULT.cookies
-# print RESULT.content
+print RESULT.content
